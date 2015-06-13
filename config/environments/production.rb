e48b051f53f77e60e230fbe7e7e66700bf8eb279
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-12 21:01:14 -0600

    fucking with precompiling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,4 @@
 Rails.application.configure do
-  #stackoverflow solution to rails 4 images not showing up
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-  config.assets.compile = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -33,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
