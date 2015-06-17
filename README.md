c5455695ef27456284c10b33507d60660bd2cee7
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-17 15:24:28 -0600

    cleaning things up

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 #tweetHaiku
+Check it out at tweethaik.us
 
 ###What is it?
+tweetHaiku is a Rails app I made in my spare time that parses the twitter feed and finds tweets that are accidentally haikus (i.e., have a 5/7/5 syllable breakdown). The best of these tweets are then posted to tweethaik.us where users can log in to upvote them and view the top tweets. I used Rails for the site with Ruby for the actual twitter API parsing, plus materialize.css to help styling.
+
 
 ###How does it work?
+A little Ruby script I wrote parses the twitter feed using the twitter gem. It filters tweets first by only those written in english (I was using the whatlanguage gem, but I'm transitioning it to il8n as I'm interested in internationalization), then filters out tweets with non-syllabic symbols such as "@", "RT", and URLs. The Rails app is a simple CRUD app with authentication and authorization.
 
 ###Can I contribute?
+Yes! Please submit a pull request, or [drop me a line](http://lev.io).
 
 ###Moving forwards
+Like I said, I want to start using the il8n gem instead of the whatlanguage gem. I also want to increase the filter quality to a degree where I no longer have to get rid of tweets that slip by my criteria. What I'm most afraid of is perfectly valid tweethaikus being posted that are incredibly offensive - I've found a number of tweets that I feel entirely uncomfortable posting. But this endeavor has been made worth it by the rest of the tweets, which cover the emotional spectrum ranging from tragic to funny to profound.
--- Services/haiku.rb
+++ Services/haiku.rb
@@ -6,7 +6,7 @@ class SyllableCount
 
   def count(text)
     count = 0
-    Syllables.new(text.downcase).to_h.each do | word, val |
+    Syllables.new(text.downcase).to_h.each do |word, val|
       count += val
     end
   end
@@ -29,29 +29,14 @@ class TweetParser
     @tweet_count = SyllableCount.new
     @client.filter(track: CHARS) do |tweet|
       if tweet.text.language == :english &&  @tweet_count.count(tweet.text).values.reduce(:+) == 17
-        puts tweet.text unless tweet.text.include?("@") || tweet.text.include?("/\d/") || tweet.text.include?("http") || tweet.text.include?("#")
+        puts tweet.text unless
+        tweet.text.include?("@") || tweet.text.include?("/\d/") || tweet.text.include?("http") || tweet.text.include?("#")
       end
     end
   end
 end
 
-x = TweetParser.new
-x.filter
+feed = TweetParser.new
+feed.filter
 
-#a = SyllableCount.new
-#puts a.count("I don't tweet much but in this moment I feel like saying Won't he do it!").values.reduce(:+)
 
-#Many people lose the small joys in the hope for the big happiness
-#This is the kind of woman I would want to be with in partnership.
-#You see that tweet and instantly wonder " Who's responsible for this ?? "
-#Hurry up September! I'm trying to be done with school already
-#can i charm box of pasta with a spell so my pipi can get licked
-#Worry is a form of praying for what you don't want ~ Bhagavan Das
-#How you spend your day determines if you'll have a good workout or not.
-#don't know what's it like to be the most hated? ask me, I'll fill ya in.
-#I wish there was a pill I can take to make me not upset and stressed..
-#Justin Bieber's mom wanted to be an actress when she was younger
-#Can today please end the way I need it to. That'd be magnificent
-#I hate waiting for my fake tan to dry before I can get in to bed
-#Getting a bloody nose at work is about as classy as it gets
-#Let's count how many people give me looks for my music being loud
